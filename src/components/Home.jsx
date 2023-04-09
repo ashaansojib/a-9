f77ea54bb3cb7cdd9937a82eac676ea42f1cc97a
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-04-10 03:11:17 +0600

    add job card and show all data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import JobsCat from './JobsCat';
+import JobsItem from './JobsItem';
 
 const Home = () => {
     // category data fetch
@@ -10,13 +11,48 @@ const Home = () => {
             .then(res => res.json())
             .then(data => setCategories(data))
     }, [])
+
+    // jobs item data fetcing
+    const [jobs, setJobs] = useState([]);
+    useEffect(() => {
+        fetch('job_data.json')
+            .then(res => res.json())
+            .then(data => setJobs(data))
+    }, [])
     return (
         <div>
             <Header></Header>
-            <section className='lg:grid grid-cols-4 gap-4 py-4'>
-                {
-                    categories.map(category => <JobsCat category={category} key={category.id}></JobsCat>)
-                }
+            <section className='py-4'>
+                <div className='text-center pb-4'>
+                    <h2 className='text-4xl font-bold'>Available Job Category List</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias optio officiis id pariatur repudiandae aspernatur, sit voluptate illum? Cum, temporibus.</p>
+                </div>
+                <div className='lg:grid grid-cols-4 gap-4 '>
+                    {
+                        categories.map(category => <JobsCat
+                            category={category}
+                            key={category.id}
+                        ></JobsCat>)
+                    }
+                </div>
+            </section>
+            {/* featured job section */}
+            <section className='py-4'>
+                <div className='text-center pb-4'>
+                    <h2 className='text-4xl font-bold'>Select and Choose Your Jobs</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias optio officiis id pariatur repudiandae aspernatur, sit voluptate illum? Cum, temporibus.</p>
+                </div>
+                <div className='lg:w-[900px] mx-auto lg:grid grid-cols-2 gap-5 justify-around'>
+                    {
+                        jobs.map(job => <JobsItem
+                            key={job.id}
+                            job={job}
+                        ></JobsItem>)
+                    }
+                </div>
+                <div className='text-center pt-4'>
+                    <button className='my-btn'>Show All</button>
+                </div>
             </section>
         </div>
     );
---
+++ src/components/JobsItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const JobsItem = ({ job }) => {
+    const { logo, company_name, job_title, job_type, location, salary } = job;
+    return (
+        <div>
+            <div className="card card-compact border shadow-xl">
+                <figure><img className='h-52 w-52 m-10' src={logo} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{job_title}</h2>
+                    <p className='text-xl'>{company_name}</p>
+                    <button className="btn btn-outline btn-success w-40">{job_type}</button>
+                    <div className='flex items-center'>
+                        <p className='flex items-center'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                        </svg>
+                            {location}</p>
+                        <p>$ {salary}</p>
+                    </div>
+                    <div className="card-actions">
+                        <button className="my-btn">View Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default JobsItem;
