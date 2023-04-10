d6b71303767c43f2ccb3ccc714d21db3a0ce6e7f
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-04-09 23:33:59 -0700

    complate the details page data

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -3,10 +3,31 @@ import { useLoaderData } from 'react-router-dom';
 
 const JobDetails = () => {
     const details = useLoaderData();
-    console.log(details)
+    const {company_name, experience, job_info, job_res, job_title, location, requirements, salary, email, phone} = details;
     return (
-        <div>
-            <h2>{details.company_name}</h2>
+        <div className='lg:grid grid-cols-3 p-4 relative'>
+            <div className='col-span-2 pr-4 space-y-4'>
+                <p><span className='font-semibold'>Job Desceiption: </span>{job_info}</p>
+                <p><span className='font-semibold'>Job Responsiblity: </span>{job_res}</p>
+                <p className='font-semibold'>Educational Requirments:</p>
+                <p>{requirements}</p>
+                <p className='font-semibold'>Experiences: </p>
+                <p>{experience}</p>
+            </div>
+            <aside className='p-4 rounded-sm bg-slate-100 absolute right-0 top-9'>
+                <div className='space-y-2'>
+                    <h2 className='font-bold text-xl border-b-2 py-2'>Job Detals</h2>
+                    <p><span className='font-semibold'>Job Title</span>: {job_title}</p>
+                    <p><span className='font-semibold'>Company</span>: {company_name}</p>
+                    <p><span className='font-semibold'>Salary</span>: {salary}</p>
+                </div>
+                <div className='space-y-2'>
+                    <h2 className='font-bold text-xl border-b-2 py-2'>Contact Information</h2>
+                    <p><span className='font-semibold'>Phone</span>: {phone}</p>
+                    <p><span className='font-semibold'>Email</span>: {email}</p>
+                    <p><span className='font-semibold'>Address</span>: {location}</p>
+                </div>
+            </aside>
         </div>
     );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <div className='lg:flex justify-between p-2'>
-            <h2 className='font-bold text-2xl'>Jobs Hunter</h2>
+            <Link to="/"> <h2 className='font-bold text-2xl'>Jobs Hunter</h2></Link>
             <ul>
                 <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')} to="/">Home</NavLink>
                 <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')} to="/static">Statistic</NavLink>
